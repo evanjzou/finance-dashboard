ebf7887c413ccb6e2fcdd72cc6411fc813e0599f
Christopher Borst <ChristopherBorst@Christophers-MacBook-Pro.local>
2017-08-09 15:57:28 -0400

    added the financial sector to the dashboard

--- constants.js
+++ constants.js
@@ -1,4 +1,5 @@
 exports.companies = [
+    //TECH SECTOR
     'ACN',
     'ATVI',
     'ADBE',
@@ -68,3 +69,189 @@ exports.companies = [
     'XLNX',
     'XRX'
 ]
+
+exports.financeCompanies =[
+    // FINANCIAL SECTOR
+    'AMG',
+    'AFL',
+    'ALL',
+    'AXP',
+    'AXP',
+    'AIG',
+    'AMP',
+    'AON',
+    'AJG',
+    'AIZ',
+    'BAC',
+    'BBT',
+    'BRK-B',
+    'BLK',
+    'HRB',
+    'COF',
+    'CBOE',
+    'SCHW',
+    'CB',
+    'CINF',
+    'C',
+    'CFG',
+    'CME',
+    'CMA',
+    'DFS',
+    'ETFC',
+    'FITB',
+    'BEN',
+    'GS',
+    'HIG',
+    'HBAN',
+    'ICE',
+    'IVZ',
+    'JPM',
+    'KEY',
+    'LUK',
+    'LNC',
+    'L',
+    'MTB',
+    'MMC',
+    'MET',
+    'MCO',
+    'MS',
+    'NDAQ',
+    'NAVI',
+    'NTRS',
+    'PBCT',
+    'PNC',
+    'PFG',
+    'PGR',
+    'PRU',
+    'RF',
+    'SPGI',
+    'STT',
+    'STI',
+    'SYF',
+    'TROW',
+    'BK',
+    'TRV',
+    'TMK',
+    'USB',
+    'UNM',
+    'WFC',
+    'WLTW',
+    'XL',
+    'ZION'
+    //END FINANCIAL SECTOR
+]
+
+
+
+
+
+
+
+/*
+exports.sectorLookup = {
+    'ACN' : "TECH",
+    'ATVI':"TECH",
+    'ADBE' : "TECH",
+    'AMD' : "TECH",
+    'AKAM' : "TECH",
+    'ADS': "TECH",
+    'GOOGL': "TECH",
+    'GOOG': "TECH",
+    'APH': "TECH",
+    'ADI': "TECH",
+    'ANSS': "TECH",
+    'AAPL': "TECH",
+    'AMAT': "TECH",
+    'ADSK': "TECH",
+    'ADP': "TECH",
+    'AVGO': "TECH",
+    'CA': "TECH",
+    'CSCO': "TECH",
+    'CTXS': "TECH",
+    'CTSH': "TECH",
+    'GLW': "TECH",
+    'CSRA': "TECH",
+    'DXC': "TECH",
+    'EBAY': "TECH",
+    'EA': "TECH",
+    'FFIV': "TECH",
+    'FB': "TECH",
+    'FIS': "TECH",
+    'FISV': "TECH",
+    'FLIR': "TECH",
+    'IT': "TECH",
+    'GPN': "TECH",
+    'HRS': "TECH",
+    'HPE': "TECH",
+    'HPQ': "TECH",
+    'INTC': "TECH",
+    'IBM': "TECH",
+    'INTU': "TECH",
+    'JNPR': "TECH",
+    'KLAC': "TECH",
+    'LRCX': "TECH",
+    'MA': "TECH",
+    'MCHP': "TECH",
+    'MU': "TECH",
+    'MSFT': "TECH",
+    'MSI': "TECH",
+    'NTAP': "TECH",
+    'NVDA': "TECH",
+    'ORCL': "TECH",
+    'PAYX': "TECH",
+    'PYPL': "TECH",
+    'QRVO': "TECH",
+    'QCOM': "TECH",
+    'RHT': "TECH",
+    'CRM': "TECH",
+    'STX': "TECH",
+    'SWKS': "TECH",
+    'SYMC': "TECH",
+    'SNPS': "TECH",
+    'TEL': "TECH",
+    'TXN': "TECH",
+    'TSS': "TECH",
+    'VRSN': "TECH",
+    'V': "TECH",
+    'WDC': "TECH",
+    'WU': "TECH",
+    'XLNX': "TECH",
+    'XRX': "TECH"
+}
+*/
+
+
+//INITIALIZING COMPANIES IN TECH SECTOR
+/* var sectors = {};
+
+for(var i =0; i <exports.companies.length; i++) {
+    sectors[exports.companies[i]] = 'TECH';
+}
+*/
+exports.sectorLookup ={}
+for(var i =0; i <exports.companies.length; i++) {
+    exports.sectorLookup[exports.companies[i]] = 'TECH';
+}
+
+//INITIALIZING COMPANIES IN FINANCIAL SECTOR
+
+
+for(var i =0; i <exports.financeCompanies.length; i++) {
+    exports.sectorLookup[exports.financeCompanies[i]] = 'FINA';
+}
+
+
+
+
+
+
+
+//for loop goes here
+//test dashboard
+var keys = Object.keys(exports.sectorLookup)
+ console.log(keys);
+
+ for (var i = 0; i < keys.length; i++) {
+     console.log(exports.sectorLookup[keys[i]]);
+ }
+    
