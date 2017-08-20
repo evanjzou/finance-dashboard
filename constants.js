14b9f2963baa5f7623f51bdcbf2afec93efc48ff
Evan Zou <evanjzou@gmail.com>
2017-08-20 16:24:11 -0400

    added constants to include companies from all sectors

--- constants.js
+++ constants.js
@@ -1,3 +1,23 @@
+/**
+ * SECTOR ABBREVIATIONS:
+ * 
+ * TECH: technology
+ * FINA: financial
+ * HECA: healthcare
+ * COND: consumer discretionary
+ * ENER: energy
+ * INDU: industrials
+ * MATE: materials
+ * UTIL: utilities
+ * REAL: real estate
+ * CONS: consumer staples
+ * TELC: telecommunications
+ * 
+ * @author Evan Zou
+ * @author Christopher Borst
+ */
+
+
 exports.companies = [
     'ACN',
     'ATVI',
@@ -140,14 +160,517 @@ exports.financeCompanies =[
     //END FINANCIAL SECTOR
 ]
 
+exports.healthCareCompanies = [
+    'ABT', 
+    'ABBV',
+    'AET',
+    'A',
+    'ALXN',
+    'AGN',
+    'ABC',
+    'AMGN',
+    'ANTM',
+    'BCR',
+    'BAX',
+    'BDX',
+    'BIIB',
+    'BSX',
+    'BMY',
+    'CAH',
+    'CELG',
+    'CNC',
+    'CERN',
+    'CI',
+    'DHR',
+    'XRAY',
+    'EW',
+    'EVHC',
+    'ESRX',
+    'GILD',
+    'HCA',
+    'HSIC',
+    'HOLX',
+    'HUM',
+    'IDXX',
+    'ILMN',
+    'INCY',
+    'ISRG',
+    'JNJ',
+    'LH',
+    'LLY',
+    'MNK',
+    'MCK',
+    'MDT',
+    'MRK',
+    'MTD',
+    'MYL',
+    'PDCO',
+    'PKI',
+    'PRGO',
+    'PFE',
+    'DGX',
+    'REGN',
+    'SYK',
+    'COO',
+    'TMO',
+    'UNH',
+    'UHS',
+    'VAR',
+    'VRTX',
+    'WAT',
+    'ZBH',
+    'ZTS'
+
+]
+
+exports.consumerDiscretionary = [
+    'AAP',
+    'MO',
+    'AMZN',
+    'ADM',
+    'AN',
+    'AZO',
+    'BBBY',
+    'BBY',
+    'BWA',
+    'BF.B',  //CHECK TO SEE IF THIS IS A .  OR  -
+    'CPB',
+    'KMX',
+    'CCL',
+    'CBS',
+    'CHTR',
+    'CMG',
+    'CHD',
+    'COH',
+    'KO',
+    'CL',
+    'CMCSA',
+    'CAG',
+    'STZ',
+    'COST',
+    'COTY',
+    'CVS',
+    'DHI',
+    'DRI',
+    'DLPH',
+    'DISCA',
+    'DISCK',
+    'DG',
+    'DLTR',
+    'DPS',
+    'EL',
+    'EXPE',
+    'FL',
+    'F',
+    'GPS',
+    'GRMN',
+    'GIS',
+    'GM',
+    'GPC',
+    'GT',
+    'HBI',
+    'HOG',
+    'HAR',
+    'HAS',
+    'HD',
+    'HRL',
+    'IPG',
+    'SJM',
+    'K',
+    'KMB',
+    'KSS',
+    'KHC',
+    'KR',
+    'LB',
+    'LEG',
+    'LEN',
+    'LKQ',
+    'LOW',
+    'M',
+    'MAR',
+    'MAT',
+    'MKC',
+    'MCD',
+    'MJN',
+    'KORS',
+    'MHK',
+    'TAP',
+    'MDLZ',
+    'MNST',
+    'NWL',
+    'NWSA',
+    'NWS',
+    'NKE',
+    'JWN',
+    'ORLY',
+    'OMC',
+    'PEP',
+    'PM',
+    'RL',
+    'PCLN',
+    'PG',
+    'PHM',
+    'PVH',
+    'RAI',
+    'ROST',
+    'RCL',
+    'SNI',
+    'SIG',
+    'SNA',
+    'SWK',
+    'SPLS',
+    'SBUX',
+    'SYY',
+    'TGT',
+    'TGNA',
+    'CLX',
+    'HSY',
+    'DIS',
+    'TIF',
+    'TWX',
+    'TJX',
+    'TSCO',
+    'TRIP',
+    'FOXA',
+    'FOX',
+    'TSN',
+    'ULTA',
+    'UA',
+    'UAA',
+    'URBN',
+    'VFC',
+    'VIAB',
+    'WMT',
+    'WBA',
+    'WHR',
+    'WFM',
+    'WYN',
+    'WYNN',
+    'YUM'
+]
+
+exports.energy = [
+    'APC',
+    'APA',
+    'BHI',
+    'COG',
+    'CNP',
+    'CHK',
+    'CVX',
+    'XEC',
+    'CMS',
+    'CXO',
+    'COP',
+    'DVN',
+    'DTE',
+    'DUK',
+    'EOG',
+    'EQT',
+    'ES',
+    'XOM',
+    'FE',
+    'FTI',
+    'HAL',
+    'HP',
+    'HES',
+    'KMI',
+    'MRO',
+    'MPC',
+    'MUR',
+    'NOV',
+    'NFX',
+    'NEE',
+    'NBL',
+    'NRG',
+    'OXY',
+    'OKE',
+    'PSX',
+    'PXD',
+    'RRC',
+    'SLB',
+    'SRE',
+    'SWN',
+    'TSO',
+    'RIG',
+    'VLO',
+    'WEC',
+    'WMB',
+    'XEL'
+]
+
+//INDUSTRIALS
+
+exports.industrials = [
+    'MMM',
+    'AYI',
+    'ALK',
+    'ALLE',
+    'AAL',
+    'AME',
+    'ARNC',
+    'BA',
+    'CHRW',
+    'CAT',
+    'CF',
+    'CTAS',
+    'CSX',
+    'CMI',
+    'DE',
+    'DAL',
+    'DOV',
+    'DNB',
+    'ETN',
+    'EMR',
+    'EFX',
+    'EXPD',
+    'FAST',
+    'FDX',
+    'FLS',
+    'FLR',
+    'FTV',
+    'FBHS',
+    'GD',
+    'GE',
+    'GWW',
+    'HAR',
+    'HON',
+    'ITW',
+    'IR',
+    'JBHT',
+    'JEC',
+    'JCI',
+    'KSU',
+    'LLL',
+    'LMT',
+    'MAS',
+    'MHK',
+    'NLSN',
+    'NSC',
+    'NOC',
+    'PCAR',
+    'PH',
+    'PNR',
+    'PPG',
+    'PWR',
+    'RTN',
+    'RSG',
+    'RHI',
+    'ROK',
+    'COL',
+    'ROP',
+    'R',
+    'LUV',
+    'SRCL',
+    'TXT',
+    'TDG',
+    'UNP',
+    'UAL',
+    'UPS',
+    'URI',
+    'UTX',
+    'VRSK',
+    'WM',
+    'XYL'
+
+]
+
+exports.materials = [
+    'APD',
+    'ALB',
+    'AMAT',
+    'AVY',
+    'BLL',
+    'CF',
+    'DOW',
+    'DD',
+    'EMN',
+    'ECL',
+    'FMC',
+    'FCX',
+    'IP',
+    'IFF',
+    'LYB',
+    'MLM',
+    'MON',
+    'NEM',
+    'NUE',
+    'PPG',
+    'PX',
+    'SEE',
+    'SHW',
+    'MOS',
+    'VMC',
+    'WRK'
+]
+
+exports.utilities =[ 
+    'AES',
+    'LNT',
+    'AEE',
+    'AEP',
+    'AWK',
+    'CNP',
+    'CMS',
+    'ED',
+    'D',
+    'DTE',
+    'DUK',
+    'EIX',
+    'ETR',
+    'ES',
+    'EXC',
+    'FE',
+    'NEE',
+    'NI',
+    'NRG',
+    'PCG',
+    'PNW',
+    'PPL',
+    'PEG',
+    'SCG',
+    'SRE',
+    'SO',
+    'WEC',
+    'XEL'
+]
+
+exports.realEstate =[
+    'AMT',
+    'AIV',
+    'AVB',
+    'BXP',
+    'CBG',
+    'CCI',
+    'DLR',
+    'EQIX',
+    'EQR',
+    'ESS',
+    'EXR',
+    'FRT',
+    'GGP',
+    'HCP',
+    'HST',
+    'IRM',
+    'KIM',
+    'MAC',
+    'MAA',
+    'PLD',
+    'PSA',
+    'O',
+    'REG',
+    'SLG',
+    'SPG',
+    'UDR',
+    'VTR',
+    'VNO',
+    'HCN',
+    'WY'
+]
+
+exports.consumerStaples = [
+    'MO',
+    'ADM',
+    'BF-B',
+    'CVS',
+    'CPB',
+    'CHD',
+    'KO',
+    'CL',
+    'CAG',
+    'STZ',
+    'COST',
+    'COTY',
+    'DPS',
+    'EL',
+    'GIS',
+    'HRL',
+    'SJM',
+    'K',
+    'KMB',
+    'KHC',
+    'KR',
+    'MKC',
+    'MJN',
+    'TAP',
+    'MDLZ',
+    'PEP',
+    'PM',
+    'PG',
+    'RAI',
+    'SPLS',
+    'SYY',
+    'CLX',
+    'HSY',
+    'TSN',
+    'WMT',
+    'WBA',
+    'WFM'
+]
+
+exports.teleComm =[
+    'T',
+    'CTL',
+    'FTR',
+    'LVLT',
+    'VZ'
+]
+
+exports.allCompanies = exports.companies + exports.financeCompanies + 
+exports.healthCareCompanies + exports.consumerDiscretionary + exports.energy + exports.industrials + 
+exports.materials + exports.utilities +  exports.realEstate + exports.consumerStaples + exports.teleComm;
+
+
 exports.sectorLookup ={}
 for(var i =0; i <exports.companies.length; i++) {
     exports.sectorLookup[exports.companies[i]] = 'TECH';
 }
 
-//INITIALIZING COMPANIES IN FINANCIAL SECTOR
-
-
 for(var i =0; i <exports.financeCompanies.length; i++) {
     exports.sectorLookup[exports.financeCompanies[i]] = 'FINA';
 }
+
+for(var i =0; i <exports.healthCareCompanies.length; i++) {
+    exports.sectorLookup[exports.healthCareCompanies[i]] = 'HECA';
+}
+
+for(var i =0; i <exports.consumerDiscretionary.length; i++) {
+    exports.sectorLookup[exports.consumerDiscretionary[i]] = 'COND';
+}
+
+for(var i =0; i <exports.energy.length; i++) {
+    exports.sectorLookup[exports.energy[i]] = 'ENER';
+}
+
+for(var i =0; i <exports.industrials.length; i++) {
+    exports.sectorLookup[exports.industrials[i]] = 'INDU';
+}
+
+for(var i =0; i <exports.materials.length; i++) {
+    exports.sectorLookup[exports.materials[i]] = 'MATE';
+}
+
+for(var i =0; i <exports.utilities.length; i++) {
+    exports.sectorLookup[exports.utilities[i]] = 'UTIL';
+}
+
+for(var i =0; i <exports.realEstate.length; i++) {
+    exports.sectorLookup[exports.realEstate[i]] = 'REAL';
+}
+
+for(var i =0; i <exports.consumerStaples.length; i++) {
+    exports.sectorLookup[exports.consumerStaples[i]] = 'CONS';
+}
+
+for(var i =0; i <exports.teleComm.length; i++) {
+    exports.sectorLookup[exports.teleComm[i]] = 'TELC';
+}
+
+//TEST
+
+
+/*var keys = Object.keys(exports.sectorLookup)
+console.log(keys);
+
+for (var i = 0; i < keys.length; i++) {
+    console.log(exports.sectorLookup[keys[i]]);
+} //*/
